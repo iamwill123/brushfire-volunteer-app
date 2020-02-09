096fafedcb28dd45f59d4d25cf629fa5a638b514
William Pei Yuan <williampeiyuan@gmail.com>
2020-02-08 23:31:50 -0500

    Updates

---
+++ precache-manifest.c7db99953b467cf1cf2d51da5ff755b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7c8f062d75a18a66a44bc74924f990",
+    "url": "/brushfire-volunteer-app/index.html"
+  },
+  {
+    "revision": "30e33d4f08f67d1b6a37",
+    "url": "/brushfire-volunteer-app/static/js/2.b7572469.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/brushfire-volunteer-app/static/js/2.b7572469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567e26c5ef53d6ad3210",
+    "url": "/brushfire-volunteer-app/static/js/main.3b875b03.chunk.js"
+  },
+  {
+    "revision": "63b73e62ef55f1dae98a",
+    "url": "/brushfire-volunteer-app/static/js/runtime-main.92a041c5.js"
+  },
+  {
+    "revision": "90acad720e265b86d70ee77d53ec2074",
+    "url": "/brushfire-volunteer-app/static/media/moon.90acad72.svg"
+  },
+  {
+    "revision": "bf4e9d455c47369014d44097a1d47c0d",
+    "url": "/brushfire-volunteer-app/static/media/sun.bf4e9d45.svg"
+  },
+  {
+    "revision": "9725d593ae0773fb6be36fcaf9cc1684",
+    "url": "/brushfire-volunteer-app/static/media/volunteer-logo.9725d593.png"
+  }
+]);
